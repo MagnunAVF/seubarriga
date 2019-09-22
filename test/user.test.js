215f2cc7216c58bba656ed89c210323b56245fce
Magnun A V F <magnunavf@gmail.com>
2019-09-22 16:04:56 -0300

    Create insert an user route

--- test/user.test.js
+++ test/user.test.js
@@ -10,3 +10,12 @@ test('Should return all users ', () => {
             expect(res.body[0]).toHaveProperty('name', 'John Doe');
         });
 });
+
+test('Should insert an user with success', () => {
+    return request(app).post('/users')
+        .send({ name: 'Walter Mitty', mail: 'walter@mail.com' })
+        .then((res) => {
+            expect(res.status).toBe(201);
+            expect(res.body.name).toBe('Walter Mitty');
+        });
+});
