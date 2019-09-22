7780f2db6d243f7b28590d0050f6cc46cd1b2a06
Magnun A V F <magnunavf@gmail.com>
2019-09-21 23:11:53 -0300

    Add jest and test example

---
+++ test/jest.test.js
@@ -0,0 +1,22 @@
+test('Example of tests with numbers', () => {
+    let number = null;
+    expect(number).toBeNull();
+
+    number = 10;
+    expect(number).not.toBeNull();
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+    expect(number).toBeGreaterThan(9);
+    expect(number).toBeLessThan(11);
+});
+
+test('Example of tests with objects', () => {
+    const obj = { name: 'John', email: 'john@mail.com' }
+    expect(obj).toHaveProperty('name');
+    expect(obj).toHaveProperty('name', 'John');
+    expect(obj.name).toBe('John');
+
+    const obj2 = { name: 'John', email: 'john@mail.com' }
+    expect(obj).toEqual(obj2);
+    expect(obj).toBe(obj);
+});
