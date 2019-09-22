cc6cd18912e076f581634daa405bda39892794bb
Magnun A V F <magnunavf@gmail.com>
2019-09-22 12:34:45 -0300

    Fix typo and improve test message

--- test/server.test.js
+++ test/server.test.js
@@ -2,6 +2,6 @@ const supertest = require('supertest');
 
 const request = supertest('http://localhost:3001')
 
-test('Must responds in 3001 port', () => {
+test('Should respond on port 3001', () => {
     return request.get('/').then(res => expect(res.status).toBe(200));
 })
