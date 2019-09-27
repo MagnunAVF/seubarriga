99ffdf6c81a535927a9f2ea6858aad434e50e798
Magnun A V F <magnunavf@gmail.com>
2019-09-27 18:59:50 -0300

    Create insert an account route and service

---
+++ src/routes/accounts.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+  const create = async (req, res) => {
+    app.services.account.save(req.body)
+      .then((result) => {
+        res.status(201).json(result[0]);
+      })
+  };
+
+  return { create }
+}
